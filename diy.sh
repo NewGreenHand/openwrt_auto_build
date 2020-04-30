3624b0bf6f8a95492a7cef0061a8f52a9d11aee2
坑飞 <myisafei@gmail.com>
2020-04-30 11:50:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,10 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+# 添加软件包 Lienol/openwrt-package
+sed -i 'src-git laohu https://github.com/huchanghui123/Lienol-openwrt-packages-backup' feeds.conf.default
+sed -i 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
 # if [ ! -d "./package/myapp" ];then
 # mkdir ./package/myapp
 #fi
