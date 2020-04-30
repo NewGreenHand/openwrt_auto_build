e955ae21e9d98578373780c7aee6f2e7b4fe026e
坑飞 <myisafei@gmail.com>
2020-04-30 09:20:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,18 +8,18 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-if [ ! -d "./package/myapp" ];then
-mkdir ./package/myapp
-fi
-cd ./package/myapp
+# if [ ! -d "./package/myapp" ];then
+# mkdir ./package/myapp
+#fi
+# cd ./package/myapp
 
 # 下载 adguard home
-echo "luci-app-adguardhome"
-if [ ! -d "./luci-app-adguardhome" ];then
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-else
-cd luci-app-adguardhome
-git pull
-cd ..
-fi
+# echo "luci-app-adguardhome"
+# if [ ! -d "./luci-app-adguardhome" ];then
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+# else
+# cd luci-app-adguardhome
+# git pull
+# cd ..
+# fi
 
